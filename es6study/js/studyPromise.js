6f1bc36a72481f2655369627450f07ef9961250c
jdkwky <752486767@qq.com>
2017-10-27 17:40:35 +0800

    webstudy
    promise

---
+++ es6study/js/studyPromise.js
@@ -0,0 +1,32 @@
+window.onload = function(argument) {
+    // var promise = new Promise(function(resolve, reject) {
+    //     if (true) {
+    //         resolve('123');
+    //     } else {
+    //         reject('error');
+    //     }
+    // });
+    // promise.then(value => {
+    //     console.log(value);
+    // }).catch(error => {
+    //     console.log(error);
+    // });
+    // console.log('in onload');
+    // var p1 = new Promise(function(resolve, reject) {
+    //     setTimeout(() => reject(new Error('fail')), 3000);
+    // 	console.log('run also');
+    // 	setTimeout(()=>console.log('promise setTimeOut'),0);
+    // });
+    // var p2 = new Promise(function(resolve, reject) {
+    //     setTimeout(() => resolve(p1), 1000);
+    // })
+    // p2.then(result => {
+    //     console.log(result)
+    // }).catch(error => {
+    //     console.log(error);
+    // })
+    // 
+    // 
+    // lazyMan
+    
+}
