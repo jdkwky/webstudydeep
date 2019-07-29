4301785c19284a290c2fa66536ec9d6ce1539959
wangkaiyuan <WANGKAIYUAN@kingsoft.com>
2019-07-29 15:35:27 +0800

    add: 测试get set 属性方法

--- base/model.js
+++ base/model.js
@@ -1,33 +1,41 @@
-class Sington{
-    constructor(name){
+class Sington {
+    constructor(name) {
         this.name = name;
     }
 }
 
-class GetSington{
-    constructor(name){
+class GetSington {
+    constructor(name) {
         this._sington = new Sington(this.name);
     }
-    get sington(){
+    get sington() {
         return this._sington;
     }
-
+    set sington(op) {}
 }
 
-
 const sington = new GetSington('wky');
-const s1 =sington.sington;
+const s1 = sington.sington;
 const s2 = sington.sington;
 const s3 = sington.sington;
 console.log('===========================');
-console.log('sington.sington1',s1);
+console.log('sington.sington1', s1);
 console.log('===========================');
 console.log('===========================');
-console.log('sington.sington2',s2);
+console.log('sington.sington2', s2);
 console.log('===========================');
 console.log('===========================');
-console.log('sington.sington3',s3);
+console.log('sington.sington3', s3);
 console.log('===========================');
 console.log('===========================');
-console.log('s1 === s2 === s3', s1 === s2, s2===s3, s1===s3);
+console.log('s1 === s2 === s3', s1 === s2, s2 === s3, s1 === s3);
+console.log('===========================');
+
+console.log('===========================');
+console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
+console.log('===========================');
+
+const s4 = new GetSington('yc').sington;
+console.log('===========================');
+console.log('sington.sington', s4 === s1);
 console.log('===========================');
