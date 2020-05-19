7b91d407acddf88484532bf2bd8d2c30b5a15eb6
Your Name <email@example.com>
2020-05-20 07:57:43 +0800

    update:增加webpack配置

--- webpackdemo/webpack.config.js
+++ webpackdemo/webpack.config.js
@@ -1,18 +1,35 @@
 // __dirname是nodejs中的一个全局变量，它指向当前执行脚本所在目录
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports={
 	devtool:'eval-source-map',
+	mode:'development',
 	entry:{
-		main: __dirname+'/app/main.js',
-		utils: __dirname+'/app/utils.js'
+		main:'./app/main.js',
+		utils:'./app/utils.js'
 	},
 	output:{
 		path:__dirname+'/static',
-		filename:'js/[name]-[chunkHash:5].js'
+		filename:'js/[name]-[chunkHash:5].js',
+		// publicPath: '../../'
 	},
 	devServer:{
 		contentBase:'./public',
 		historyApiFallback:true,
 		inline:true,
 		port:9999
-	}
+	},
+
+	plugins:[
+		new CleanWebpackPlugin({
+			verbose:true
+		}),
+		new HtmlWebpackPlugin({
+			title:'my app',
+			filename: 'index.html'
+		})
+	]
+
+
 }
