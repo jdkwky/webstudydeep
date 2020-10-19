59713ffc68b401b5eab8560412fe6a9727424fb4
wangkaiyuan <wangkaiyuan@kingsoft.com>
2020-10-19 15:09:06 +0800

    update: 测试compile钩子函数

--- webpackdemo/plugin/console.js
+++ webpackdemo/plugin/console.js
@@ -1,10 +1,21 @@
-const pluginName = "ConsoleBuildWebpackPlugin";
+const pluginName = 'ConsoleBuildWebpackPlugin';
 
 class ConsoleBuildWebpackPlugin {
-    apply(compiler){
-        compiler.hooks.run.tap(pluginName,compilation=>{
-            console.log('webpack 构建过程开始了~~~')
-        })
+    apply(compiler) {
+        compiler.hooks.run.tapPromise(pluginName, compilation => {
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    console.log(`webpack ${pluginName}构建过程开始了~~~`);
+                    resolve('webpack1');
+                }, 1000);
+            });
+        });
+        compiler.hooks.run.tapPromise(pluginName, compilation => {
+            return new Promise(resolve => {
+                console.log(`webpack ${pluginName}构建进行中~~~`);
+                resolve('webpack2');
+            });
+        });
     }
 }
 
